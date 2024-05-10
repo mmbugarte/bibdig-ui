2a7013f12624ee67b4a562f1d9759768dc8a16a1
Nicolás Passerini <npasserini@gmail.com>
2024-05-09 23:08:35 -0300

    Reemplazo datos hard-coded por api

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,28 @@
 <script setup lang="ts">
-import { defineProps } from "vue";
+import { ref, onMounted } from "vue";
 
-const books = [
-  {
-    id: 1,
-    title: "El gran Gatsby",
-    author: "F. Scott Fitzgerald",
-    year: 1925,
-    image: "https://via.placeholder.com/150",
-    downloadUrl: "https://example.com/book1.pdf",
-  },
-  {
-    id: 2,
-    title: "Orgullo y prejuicio",
-    author: "Jane Austen",
-    year: 1813,
-    image: "https://via.placeholder.com/150",
-    downloadUrl: "https://example.com/book2.pdf",
-  },
-  {
-    id: 3,
-    title: "Matar a un ruiseñor",
-    author: "Harper Lee",
-    year: 1960,
-    image: "https://via.placeholder.com/150",
-    downloadUrl: "https://example.com/book3.pdf",
-  },
-  {
-    id: 4,
-    title: "1984",
-    author: "George Orwell",
-    year: 1949,
-    image: "https://via.placeholder.com/150",
-    downloadUrl: "https://example.com/book4.pdf",
-  },
-  {
-    id: 5,
-    title: "Don Quijote de la Mancha",
-    author: "Miguel de Cervantes",
-    year: 1605,
-    image: "https://via.placeholder.com/150",
-    downloadUrl: "https://example.com/book5.pdf",
-  },
-  {
-    id: 6,
-    title: "Cien años de soledad",
-    author: "Gabriel García Márquez",
-    year: 1967,
-    image: "https://via.placeholder.com/150",
-    downloadUrl: "https://example.com/book6.pdf",
-  },
-  {
-    id: 7,
-    title: "Ulises",
-    author: "James Joyce",
-    year: 1922,
-    image: "https://via.placeholder.com/150",
-    downloadUrl: "https://example.com/book7.pdf",
-  },
-  {
-    id: 8,
-    title: "La Odisea",
-    author: "Homero",
-    year: "Siglo VIII a.C.",
-    image: "https://via.placeholder.com/150",
-    downloadUrl: "https://example.com/book8.pdf",
-  },
-];
+interface Libro {
+  id: number;
+  titulo: string;
+  autor: string;
+  publicado: number;
+  urlImagenPortada: string;
+  urlDownload: string;
+}
+
+const libros = ref<Libro[]>([]);
+
+const fetchData = async () => {
+  try {
+    const respuesta = await fetch("/api/libros");
+    const datos = await respuesta.json();
+    libros.value = datos;
+  } catch (error) {
+    console.error("Error al recuperar los datos:", error);
+  }
+};
+
+onMounted(fetchData);
 </script>
 
 <template>
@@ -80,15 +36,19 @@ const books = [
       />
     </div>
     <div class="row">
-      <div class="col-lg-4 col-md-6" v-for="book in books" :key="book.id">
+      <div class="col-lg-4 col-md-6" v-for="libro in libros" :key="libro.id">
         <div class="card mb-4">
-          <img :src="book.image" class="card-img-top" alt="Portada del libro" />
+          <img
+            :src="libro.urlImagenPortada"
+            class="card-img-top"
+            alt="Portada del libro"
+          />
           <div class="card-body">
-            <h5 class="card-title">{{ book.title }}</h5>
-            <h6 class="card-subtitle mb-2 text-muted">{{ book.author }}</h6>
-            <p class="card-text">Año: {{ book.year }}</p>
+            <h5 class="card-title">{{ libro.titulo }}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">{{ libro.autor }}</h6>
+            <p class="card-text">Año: {{ libro.publicado }}</p>
             <a
-              :href="book.downloadUrl"
+              :href="libro.urlDownload"
               class="btn btn-primary"
               target="_blank"
               rel="noopener noreferrer"
--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,27 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     allowedHosts: "all",
+    proxy: {
+      "/api": {
+        target: "http://cms:1337/api", // La URL de tu servidor Strapi
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api": "", // Elimina la ruta /api del inicio de la URL
+        },
+        on: {
+          proxyReq: (proxyReq, req, res) => {
+            // Registrar el pedido (request) a la consola
+            console.log(`Solicitud a la API: ${req.method} ${req.path}`);
+          },
+          error: (err, req, res) => {
+            console.error("Error en proxy:", err);
+            res.writeHead(500, {
+              "Content-Type": "text/plain",
+            });
+            res.end("Proxy error.");
+          },
+        },
+      },
+    },
   },
 });
