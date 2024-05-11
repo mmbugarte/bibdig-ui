c5d3bb12f2fecc1409553e7fec137a02b1e1f0f5
Nicolás Passerini <npasserini@gmail.com>
2024-05-11 20:17:38 -0300

    Saco la config a .env.

--- vue.config.js
+++ vue.config.js
@@ -5,14 +5,14 @@ module.exports = defineConfig({
     allowedHosts: "all",
     proxy: {
       "/api": {
-        target: "http://cms:1337", // La URL de tu servidor Strapi
+        target: process.env.VUE_APP_API_URL, // "http://cms:1337", // La URL de tu servidor Strapi
         changeOrigin: true,
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         onProxyReq: (proxyReq, req, res) => {
           console.log(`API request proxied: ${req.method} ${req.path}`);
           proxyReq.setHeader(
             "Authorization",
-            "Bearer a5b8da0497aacfb9a56baff3a54e8deaaa33a96739f7ae4e365f6ffe31bf4d01239ffc7e28aa0351e380738421c8904272aca00f7ee2e28e2b36c7edb944b71cc45e343cb1a4e2f30f5b0c588bc189f418848891af331c84bab7725dfd175d756ddb6e180e655478f38e3dd881b593d85970d0b9649c9724a99b168e9fa29caa"
+            `Bearer ${process.env.VUE_APP_STRAPI_API_TOKEN}`
           );
         },
       },
